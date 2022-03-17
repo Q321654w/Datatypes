53b5475076b58ab5ed7d150559dc9dde28090599
Q321654w <80633262+Q321654w@users.noreply.github.com>
2022-03-17 17:48:49 +0200

    Refactroring

--- Datatypes/BitInByte.cs
+++ Datatypes/BitInByte.cs
@@ -8,16 +8,5 @@
         {
             return BIT_IN_BYTE;
         }
-
-        public int BitsCount(byte value)
-        {
-            for (var i = BIT_IN_BYTE - 1; i >= 0; i--)
-            {
-                if((value >> i & 1) == 1)
-                    return i + 1;
-            }
-
-            return 0;
-        }
     }
 }
--- Datatypes/IntConverter.cs
+++ Datatypes/IntConverter.cs
@@ -65,11 +65,8 @@
             var count = 0;
             while (value > 0)
             {
-                if ((value & 1) != 1)
-                    continue;
-
-                value >>= 1;
                 count++;
+                value >>= 1;
             }
 
             return count;
